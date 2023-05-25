6b94a10716a6d7e42f813ac7cf4ed6fdc11c6a33
jahid morol <jahidmorol2@gmail.com>
2023-05-25 15:25:19 +0600

    install some pakage and implant navbar and banner section

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import Home from "../pages/home/home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
